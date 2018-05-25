c48f436b7020ae8e1eb7c450bfe2962a8a798828
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-05-25 14:04:03 +0100

    Make script more generic

--- apply_rnd_settings.sh
+++ apply_rnd_settings.sh
@@ -9,22 +9,34 @@
 # Note: 
 # - You need to log in first before running this script.
 # - If any of the datasets has more than 100 images increase the --limit parameter!
+#
+# This script applies the rendering settings using the render plugin
+# It is done by a third party person for all the users
+FILE=${FILE:-renderingMapping.tsv}
+OMEROPATH=${OMEROPATH:-/opt/omero/server/OMERO.server/bin/omero}
+PASSWORD=${PASSWORD:-ome}
+HOST=${HOST:-outreach.openmicroscopy.org}
+NUMBER=${NUMBER:-40}
+OMEUSER=${OMEUSER:-user}
+SUDOER=${SUDOER:-importer1}
 
-file="idr0021-subset-experimentA-renderingMapping.tsv"
-omero="/opt/omero/server/OMERO.server/bin/omero"
+for ((i=1;i<=$NUMBER;i++));
+do  $OMEROPATH login --sudo ${SUDOER} -u $OMEUSER-$i -s $HOST -w $PASSWORD
+    while IFS='	' read -r f1 f2
+    do
+        imageids=`$OMEROPATH hql --ids-only --limit 500 --style csv -q "select img from DatasetImageLink l join l.parent as ds join l.child as img where ds.name = '$f1'"`
+        IFS=',' read -r -a array <<< $imageids
 
-while IFS='	' read -r f1 f2
-do
-	imageids=`$omero hql --ids-only --limit 500 --style csv -q "select img from DatasetImageLink l join l.parent as ds join l.child as img where ds.name = '$f1'"`
-	IFS=',' read -r -a array <<< $imageids
-	
-	for imageid in "${array[@]}"
-	do
-		imageid=${imageid/ */}
-		if [[ $imageid == Image* ]]
-		then
-			printf 'Applying rendering settings %s (dataset %s) to %s \n' "$f2" "$f1" "$imageid"
-			$omero render edit $imageid $f2
-		fi
-	done
-done <"$file"
+        for imageid in "${array[@]}"
+        do
+            imageid=${imageid/ */}
+            if [[ $imageid == Image* ]]
+            then
+                printf 'Applying rendering settings %s (dataset %s) to %s \n' "$f2" "$f1" "$imageid"
+                $OMEROPATH render set $imageid $f2
+        fi
+        done
+    done < "$FILE"
+done
+echo Finishing
+
