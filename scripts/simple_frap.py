b9d43c482037c119ad6f01dff7d3750295e423d8
Dominik Lindner <d.lindner@dundee.ac.uk>
2020-05-19 13:12:15 +0100

    Fix flake8

--- scripts/simple_frap.py
+++ scripts/simple_frap.py
@@ -7,160 +7,158 @@ import matplotlib
 from matplotlib import pyplot as plt
 from getpass import getpass
 
-# Step 1 - Connect to OMERO server
 
+# Step 1 - Connect to OMERO server
 def connect(hostname, username, password):
-  """
-  Connect to an OMERO server
-  :param hostname: Host name
-  :param username: User
-  :param password: Password
-  :return: Connected BlitzGateway
-  """
-  conn = BlitzGateway(username, password,
-                      host=hostname, secure=True)
-  conn.connect()
-  return conn
+    """
+    Connect to an OMERO server
+    :param hostname: Host name
+    :param username: User
+    :param password: Password
+    :return: Connected BlitzGateway
+    """
+    conn = BlitzGateway(username, password,
+                        host=hostname, secure=True)
+    conn.connect()
+    return conn
 
 
 # Step 3 - Load metadata (channel information)
-
 def get_channel_index(image, label):
-  """
-  Get the channel index of a specific channel
-  :param image: The image
-  :param label:  The channel name
-  :return: The channel index (None if not found)
-  """
-  labels = image.getChannelLabels()
-  if label in labels:
-    idx = labels.index(label)
-    return idx
-  return None
+    """
+    Get the channel index of a specific channel
+    :param image: The image
+    :param label:  The channel name
+    :return: The channel index (None if not found)
+    """
+    labels = image.getChannelLabels()
+    if label in labels:
+        idx = labels.index(label)
+        return idx
+    return None
 
 
 # Step 4 - Load metadata (ellipse ROI)
-
 def get_ellipse_roi(conn, image):
-  """
-  Get the first ellipse ROI found in the image
-  :param conn: The BlitzGateway
-  :param image: The Image
-  :return: The shape ID of the first ellipse ROI found
-  """
-  roi_service = conn.getRoiService()
-  result = roi_service.findByImage(image.getId(), None)
-  shape_id = None
-  for roi in result.rois:
-    for s in roi.copyShapes():
-      if type(s) == EllipseI:
-        shape_id = s.id.val
-  return shape_id
+    """
+    Get the first ellipse ROI found in the image
+    :param conn: The BlitzGateway
+    :param image: The Image
+    :return: The shape ID of the first ellipse ROI found
+    """
+    roi_service = conn.getRoiService()
+    result = roi_service.findByImage(image.getId(), None)
+    shape_id = None
+    for roi in result.rois:
+        for s in roi.copyShapes():
+            if type(s) == EllipseI:
+                shape_id = s.id.val
+    return shape_id
 
 
 # Step 5 - Get the mean intensities
-
 def get_mean_intensities(conn, image, the_c, shape_id):
-  """
-  Get the mean pixel intensities of an roi in a time series image
-  :param conn: The BlitzGateway
-  :param image: The image
-  :param the_c: The channel index
-  :param shape_id: The ROI shape id
-  :return: List of mean intensity values (one for each timepoint)
-  """
-  roi_service = conn.getRoiService()
-  the_z = 0
-  size_t = image.getSizeT()
-  meanvalues = []
-  for t in range(size_t):
-    stats = roi_service.getShapeStatsRestricted([shape_id],
-                                                the_z, t, [the_c])
-    meanvalues.append(stats[0].mean[the_c])
-  return meanvalues
+    """
+    Get the mean pixel intensities of an roi in a time series image
+    :param conn: The BlitzGateway
+    :param image: The image
+    :param the_c: The channel index
+    :param shape_id: The ROI shape id
+    :return: List of mean intensity values (one for each timepoint)
+    """
+    roi_service = conn.getRoiService()
+    the_z = 0
+    size_t = image.getSizeT()
+    meanvalues = []
+    for t in range(size_t):
+        stats = roi_service.getShapeStatsRestricted([shape_id],
+                                                    the_z, t, [the_c])
+        meanvalues.append(stats[0].mean[the_c])
+    return meanvalues
+
 
 # Step 6 - Plot the data
-
 def plot(values, plot_filename):
-  """
-  Creates a simple plot of the given values
-  and saves it.
-  :param values: The values
-  :param plot_filename: The file name
-  :return: Nothing
-  """
-  matplotlib.use('Agg')
-  fig = plt.figure()
-  plt.subplot(111)
-  plt.plot(values)
-  fig.canvas.draw()
-  fig.savefig(plot_filename)
-  pil_img = Image.open(plot_filename)
-  pil_img.show()
+    """
+    Creates a simple plot of the given values
+    and saves it.
+    :param values: The values
+    :param plot_filename: The file name
+    :return: Nothing
+    """
+    matplotlib.use('Agg')
+    fig = plt.figure()
+    plt.subplot(111)
+    plt.plot(values)
+    fig.canvas.draw()
+    fig.savefig(plot_filename)
+    pil_img = Image.open(plot_filename)
+    pil_img.show()
+
 
 # Step 7 - Save the results
-
 def save_values(conn, image, values):
-  """
-  Attach the values as map annotation to the image
-  :param conn: The BlitzGateway
-  :param image: The image
-  :param values: The values
-  :return: Nothing
-  """
-  namespace = "demo.simple_frap_data"
-  key_value_data = [[str(t), str(value)] for t, value in enumerate(values)]
-  map_ann = MapAnnotationWrapper(conn)
-  map_ann.setNs(namespace)
-  map_ann.setValue(key_value_data)
-  map_ann.save()
-  image.linkAnnotation(map_ann)
+    """
+    Attach the values as map annotation to the image
+    :param conn: The BlitzGateway
+    :param image: The image
+    :param values: The values
+    :return: Nothing
+    """
+    namespace = "demo.simple_frap_data"
+    key_value_data = [[str(t), str(value)] for t, value in enumerate(values)]
+    map_ann = MapAnnotationWrapper(conn)
+    map_ann.setNs(namespace)
+    map_ann.setValue(key_value_data)
+    map_ann.save()
+    image.linkAnnotation(map_ann)
+
 
 # Step 8 - Save the plot
-
 def save_plot(conn, image, plot_filename):
-  """
-  Save the plot to OMERO
-  :param conn: The BlitzGateway
-  :param image: The image
-  :param plot_filename: The path to the plot image
-  :return: Nothing
-  """
-  pil_img = Image.open(plot_filename)
-  np_array = np.asarray(pil_img)
-  red = np_array[::, ::, 0]
-  green = np_array[::, ::, 1]
-  blue = np_array[::, ::, 2]
-  plane_gen = iter([red, green, blue])
-  i = conn.createImageFromNumpySeq(plane_gen, plot_filename, sizeC=3,
-                                   dataset=image.getParent())
+    """
+    Save the plot to OMERO
+    :param conn: The BlitzGateway
+    :param image: The image
+    :param plot_filename: The path to the plot image
+    :return: Nothing
+    """
+    pil_img = Image.open(plot_filename)
+    np_array = np.asarray(pil_img)
+    red = np_array[::, ::, 0]
+    green = np_array[::, ::, 1]
+    blue = np_array[::, ::, 2]
+    plane_gen = iter([red, green, blue])
+    i = conn.createImageFromNumpySeq(plane_gen, plot_filename, sizeC=3,
+                                     dataset=image.getParent())
+
 
 # Step 9 - Wrap it all up
-
 def analyse(conn, image_id, channel_name):
-  # Step 2 - Load iamge
-  img = conn.getObject("Image", image_id)
-  # -
-  ci = get_channel_index(img, channel_name)
-  shape_id = get_ellipse_roi(conn, img)
-  values = get_mean_intensities(conn, img, ci, shape_id)
-  plot(values, 'plot.png')
-  save_values(conn, img, values)
-  save_plot(conn, img, 'plot.png')
+    # Step 2 - Load iamge
+    img = conn.getObject("Image", image_id)
+    # -
+    ci = get_channel_index(img, channel_name)
+    shape_id = get_ellipse_roi(conn, img)
+    values = get_mean_intensities(conn, img, ci, shape_id)
+    plot(values, 'plot.png')
+    save_values(conn, img, values)
+    save_plot(conn, img, 'plot.png')
 
 
 def main():
-  try:
-    hostname = "wss://workshop.openmicroscopy.org/omero-ws"
-    username = input("Username: ")
-    password = getpass("Password: ")
-    image_id = int(input("Image ID: "))
-    channel = input("Channel name: ")
-    conn = connect(hostname, username, password)
-    analyse(conn, image_id, channel)
-  finally:
-    if conn:
-      conn.close()
+    try:
+        hostname = "wss://workshop.openmicroscopy.org/omero-ws"
+        username = input("Username: ")
+        password = getpass("Password: ")
+        image_id = int(input("Image ID: "))
+        channel = input("Channel name: ")
+        conn = connect(hostname, username, password)
+        analyse(conn, image_id, channel)
+    finally:
+        if conn:
+            conn.close()
+
 
 if __name__ == "__main__":
-  main()
+    main()
