38a8c7036afd63a5ac4624fcdd661f260a6ab9af
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-21 14:49:12 +0100

    add example scripts

---
+++ scripts/hello_world.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------------------------
+#   Copyright (C) 2020 University of Dundee. All rights reserved.
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from getpass import getpass
+
+# Import OMERO Python BlitzGateway
+from omero.gateway import BlitzGateway
+
+
+# Step 1
+def connect(hostname, username, password):
+    """
+    Connect to an OMERO server
+    :param hostname: Host name
+    :param username: User
+    :param password: Password
+    :return: Connected BlitzGateway
+    """
+    conn = BlitzGateway(username, password,
+                        host=hostname, secure=True)
+    conn.connect()
+    conn.c.enableKeepAlive(60)
+    return conn
+
+
+# Step 2
+def load_images(conn, dataset_id):
+    """
+    Load the images in the specified dataset
+    :param conn: The BlitzGateway
+    :param dataset_id: The dataset's id
+    :return: The Images or None
+    """
+    dataset = conn.getObject("Dataset", dataset_id)
+    images = []
+    for image in dataset.listChildren():
+        images.append(image)
+    if len(images) == 0:
+        return None
+
+    for image in images:
+        print("---- Processing image", image.id)
+    return images
+
+# main
+if __name__ == "__main__":
+    try:
+        # Collect parameters
+        host = input("Host [wss://workshop.openmicroscopy.org/omero-ws]: ") or 'wss://workshop.openmicroscopy.org/omero-ws'  # noqa
+        username = input("Username [trainer-1]: ") or 'trainer-1'
+        password = getpass("Password: ")
+        dataset_id = input("Dataset ID [2391]: ") or '2391'
+
+        # Connect to the server
+        conn = connect(host, username, password)
+
+        # Load the images container in the specified dataset
+        load_images(conn, dataset_id)
+    finally:
+        conn.close()
+    print("done")
